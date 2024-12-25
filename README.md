579a9360c4c36ceeae609855c91fa1c08d1d3904
TOGOMORIl <anonpeterhacker@outlook.com>
2024-12-25 03:07:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+"# Ecofriendly" 
+"# Ecofriendly" 
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "ecofriendlyf8516e96": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
